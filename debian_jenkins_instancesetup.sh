fd2f4c7b105c5077d144b64f245c54d517d61db7
Craig Ringer <craig@2ndquadrant.com>
2014-07-25 01:45:40 +0800

    Fix some assumptions about Debian distro version

--- debian_jenkins_instancesetup.sh
+++ debian_jenkins_instancesetup.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 #
-# This script sets up a Debian 6 instance as a Jenkins worker
+# This script sets up a Debian instance as a Jenkins worker
 # 
 # It's used for all versions and architectures. Add tests for `arch` etc
 # if you want to branch for different worker types.
 
 export DEBIAN_FRONTEND=noninteractive 
 export DEBIAN_PRIORITY=critical
-cat >  /etc/apt/sources.list <<"__END__"
-deb http://ftp.ie.debian.org/debian squeeze main
-deb-src http://ftp.ie.debian.org/debian/ squeeze main
-deb http://security.debian.org/ squeeze/updates main
-deb-src http://security.debian.org/ squeeze/updates main
-__END__
+
+# no longer required with the new AMIs:
+#cat >  /etc/apt/sources.list <<"__END__"
+#deb http://ftp.ie.debian.org/debian squeeze main
+#deb-src http://ftp.ie.debian.org/debian/ squeeze main
+#deb http://security.debian.org/ squeeze/updates main
+#deb-src http://security.debian.org/ squeeze/updates main
+#__END__
+
 apt-get -q update
-apt-get -q -y dist-upgrade
-apt-get -q -y upgrade
-apt-get -q -y install openjdk-6-jre-headless
-apt-get -q -y build-dep postgresql
-apt-get -q -y install libreadline6-dev git curl
+apt-get install aptitude
+aptitude -q2 -y upgrade
+aptitude -q2 -y install openjdk-6-jre-headless
+aptitude -q2 -y build-dep postgresql
+aptitude -q2 -y install libreadline6-dev git curl
