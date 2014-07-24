ebc9d54ee58b4ee7716c3661983e5c14a44026a4
Craig Ringer <craig@2ndquadrant.com>
2014-07-24 15:52:22 +0800

    Enable use of ephemeral storage

--- generated/amazonlinux201209_jenkins_instancesetup.sh
+++ generated/amazonlinux201209_jenkins_instancesetup.sh
@@ -40,6 +40,35 @@ JENKINS_REMOTE_FS_ROOT=/var/cache/jenkins
 # See https://issues.jenkins-ci.org/browse/JENKINS-16027
 
 mkdir -p $JENKINS_REMOTE_FS_ROOT
+
+# Set up EBS ephemeral instance store and make it the Jenkins workspace if
+# available
+ephemeral0=$(curl -f -s http://169.254.169.254/2012-01-12/meta-data/block-device-mapping/ephemeral0)
+if test -n "$ephemeral0"; then
+    # contains device node name like "sdb". The local host might use Xen style
+    # names like xvb instead; have to detect this and remap.
+    rootdev="$(df -h / | awk '/^\/dev/ { print $1 }')"
+    if test "$rootdev" = "/dev/xvda1"; then
+        ephemeral0="/dev/${ephemeral0/sd/xvd}"
+    elif test "$rootdev" = "/dev/sda1"; then
+        # No action, it's already in sdx format
+        ephemeral0="/dev/${ephemeral0}"
+    else
+        echo "Unable to determine device node format for rootdev=$rootdev"
+        ephemeral0=""
+    fi
+
+    if test -n "$ephemeral0"; then
+        echo "Formatting ephemeral volume $ephemeral0"
+        mkfs -t ext4 $ephemeral0
+        echo "Mounting $ephemeral0 on $JENKINS_REMOTE_FS_ROOT"
+        mount -t ext4 -o noatime,data=writeback,barrier=0 $ephemeral0 $JENKINS_REMOTE_FS_ROOT
+    fi
+else
+    echo "Not using ephemeral storage: no ephemeral0 mapped"
+fi
+
+# Set Jenkins root ownership
 if id ec2-user >& /dev/null ; then
     chown -R ec2-user $JENKINS_REMOTE_FS_ROOT
 elif id admin &> /dev/null; then
@@ -47,6 +76,8 @@ elif id admin &> /dev/null; then
 else
     echo "WARNING: Don't know what owner to give to $JENKINS_REMOTE_FS_ROOT"
 fi
+
+
 
 # Override DNS by adding a hosts entry for qa.2ndquadrant.com that points to
 # its EC2 private IP for within VPC. Because our DNS is at 1and1 we don't have
